305e7caf19de434a8f291b26d35fa213dea9b922
yanxiangkang <1748572489@qq.com>
2016-09-29 15:41:55 +0800

    zy

---
+++ data/timeAxis.js
@@ -0,0 +1,46 @@
+
+/*		工作时光轴
+	*   {
+	*       startTime: @value String            *         工作开始时间  格式为：2016.10.09
+	*       endTime: @value String 			    *         工作结束时间  格式为：2016.10.09
+	*       companyLogo: @value String 	        *         公司log
+			companyName: @value String          *         公司名称
+	*       companyWebsite: @value String       []        公司网址       格式为：http://www.miaov.com
+	*       jobPost: @value String              *         工作职位
+	*       jonTask: @value String       		*         负责任务
+	*       jobContent: @value String         	*         工作具体内容
+	*
+	*
+	*   }
+*/
+
+var timeAxisArr = [
+		{
+		    startTime:"2015.09.01",
+			endTime:"2016.03.01",
+			companyLogo:'images/icon/expre_logo4.png',
+			companyNam:"北京中科康达医院管理中心",
+			companyWebsite:"http://www.miaov.com",
+			jobPost:"网站编辑",
+			jonTask:"网站图片，文章的上传",
+			jobContent:"包括：制定前端静态资源目录结构和代码发布流程；根据前者开发代码构建工具，简化发布操作；开发前后端中间平台，分离前后端职责，提高开发效率。以及PC网站的开发。",
+		},
+		{
+		    startTime:"2016.03.15",
+			endTime:"至今",
+			companyLogo:'images/icon/expre_logo4.png',
+			companyNam:"北京盛世阳光有限公司",
+			companyWebsite:"http://www.miaov.com",
+			jobPost:"项目助理",
+			jonTask:"前端负责人。搭建了前端的整体构架",
+			jobContent:"包括：制定前端静态资源目录结构和代码发布流程；根据前者开发代码构建工具，简化发布操作；开发前后端中间平台，分离前后端职责，提高开发效率。此外，还负责美黛拉App内嵌页面和PC网站的开发。",
+		}
+//		{
+//		    startTime:"2016.01.01",
+//			endTime:"2016.01.05",
+//			companyNam:"妙味课堂",
+//			companyLogo:'images/icon/expre_logo4.png',
+//			companyWebsite:"http://www.miaov.com",
+//			jobContent:"包括：制定前端静态资源目录结构和代码发布流程；根据前者开发代码构建工具，简化发布操作；开发前后端中间平台，分离前后端职责，提高开发效率。此外，还负责美黛拉App内嵌页面和PC网站的开发。",
+//		}
+]
---
+++ data/userInfo.js
@@ -0,0 +1,37 @@
+
+/*个人信息说明，数据格式为对象
+	*   {
+	*       userName: @value String               *         姓名
+			userPortrait: @value String           *         头像
+	*       jobWant: @value String                *         求职意向
+	*       userQQ: @value String|Number          *         QQ号码
+	*       userEmail: @value String              *         邮箱地址
+	*       motto: @value String                  []         激励格言
+	*       userPhone: @value String|Number       *          手机号码
+	*       userOrignPlace: @value String         *         籍贯
+	*       userSeatPlace: @value String          *         所在地
+	*       userWeibo：@value String              []         微博地址
+	*       userAssessment: @value String         []        自我评价
+	*       userHobby: @value Array              []          爱好
+	*       userAward:@value Array               []          奖项
+	*
+	*
+	*   }
+*/
+
+var userInfo = {
+    userName: "康燕香" , 
+    userPortrait:"images/icon/intro_03.png",
+    jobWant:  "WEB前端开发工程师",
+    userQQ:  "1748572489",
+    userEmail:   "1748572489@qq.com",
+    motto:  "努力足够，你期待的自然会来。努力足够，你期待的自然会来。努力足够，你期待的自然会来。努力足够，你期待的自然会来。努力足够，你期待的自然会来。努力足够，你期待的自然会来。努力足够，你期待的自然会来。努力足够，你期待的自然会来。努力足够，你期待的自然会来。努力足够，你期待的自然会来。", 
+    userPhone: 18231359268,
+    userOrignPlace:  "河北省张家口市" ,
+    userSeatPlace:  "北京市",
+    userWeibo: "",
+    userAssessment: "永远保持一颗学习的心，积极进取，认真负责，待人和善。",   
+    userHobby: ["运动","画画","电影"],
+//  userAward: [""]
+}
+
