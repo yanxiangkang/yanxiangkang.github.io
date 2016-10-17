9d5ed1051e1dad4847d28d176bdab469cd5d43cc
yanxiangkang <1748572489@qq.com>
2016-10-17 19:06:52 +0800

    project

--- data/project.js
+++ data/project.js
@@ -14,6 +14,15 @@
 
 var projects = [
 		{
+			projectName:"汇爱婚礼网站制作",
+			projectWebsite:"yanxiangkang.github.io/yidongduan",
+		    startTime:"2016.01.01",
+			endTime:"至今",
+			projectExplain:"汇爱婚礼网站的制作主要运用了HTML和css制作完成，页面的动态效果运用的是JavaScript编写而成。整个页面的效果给人以一种美的视角展现出来。",
+			projectLabel:["html","css","js"],
+			projectThumbnail:"zuopin1/img/2.jpg"
+		},
+		{
 			projectName:"1号店-全站制作",
 			projectWebsite:"yanxiangkang.github.io/quanzhan",
 		    startTime:"2016.01.01",
@@ -30,14 +39,5 @@ var projects = [
 			projectExplain:"用户观看的主页面，其大部分内容是通过html和css加载的。通过HTML和css的布局结构完成了jquery首页的响应式的制作",
 			projectLabel:["html","css"],
 			projectThumbnail:"xiangyingshi/img/contentText_resource_imglink.jpg"
-		},
-		{
-			projectName:"淘宝网站-移动端制作",
-			projectWebsite:"yanxiangkang.github.io/yidongduan",
-		    startTime:"2016.01.01",
-			endTime:"至今",
-			projectExplain:"用户观看的主页面，其大部分内容是通过html和css加载的。通过HTML和css的布局结构完成了jquery首页的响应式的制作",
-			projectLabel:["html","css"],
-			projectThumbnail:"xiangyingshi/img/chuchuang-7.jpg"
 		}
 ]
